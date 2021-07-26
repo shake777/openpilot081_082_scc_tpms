481877744f2764d8b81528dead46ab4c53a40093
shake777 <69850594+shake777@users.noreply.github.com>
2021-07-26 13:33:25 +0900

    K7 튜닝 작업

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -51,16 +51,16 @@ class CarInterface(CarInterfaceBase):
 
     ret.lateralTuning.lqr.scale = 1700.
     ret.lateralTuning.lqr.ki = 0.01
-    ret.lateralTuning.lqr.dcGain = 0.002856
+    ret.lateralTuning.lqr.dcGain = 0.00275
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
-    ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-    ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+    ret.lateralTuning.lqr.k = [-110., 451.]
+    ret.lateralTuning.lqr.l = [0.33, 0.318]
 
     ret.steerRatio = 17.2
-    ret.steerActuatorDelay = 0.80
+    ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 3.0
     ret.steerRateCost = 0.38
     ret.steerMaxBP = [0.]
